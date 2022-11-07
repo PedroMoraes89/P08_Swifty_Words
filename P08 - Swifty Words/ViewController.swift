4d76b50aa5726e69118b2ef8e11e009928ce85dd
PEDRO HENRIQUE DE OLIVEIRA MORAES <86898701+PedroMoraes89@users.noreply.github.com>
2022-11-07 05:37:17 -0800

    Initial Commit

---
+++ P08 - Swifty Words/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  P08 - Swifty Words
+//
+//  Created by user220265 on 11/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
